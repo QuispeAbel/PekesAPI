237f56773fdc2fab0a5c5214da38f47540fa8c32
QuispeAbel <quisperobledoabel@gmail.com>
2026-02-12 15:29:50 -0300

    issue #1.5 Creación de controller healt para probar que la API funciona

---
+++ PekesAPI/PekesAPI/Controllers/HealtController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Pekes.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealtController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<OkObjectResult> GetHealt()
+        {
+            return Ok("It's live");
+        }
+    }
+}
