3ab8ef08c05027223cb2c4fe9f483d5f64555376
QuispeAbel <quisperobledoabel@gmail.com>
2026-02-23 14:50:05 -0300

    issue #2.7: Registro del DbContext en Infrastructure y referencia a la clase en Program.cs del proyecto API

---
+++ PekesAPI/Pekes.Infrastructure/DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Pekes.Infrastructure.Persistence;
+
+namespace Pekes.Infrastructure.DependencyInjection
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddInfrastructure(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<PekesDbContext>(options =>
+                options.UseSqlServer(
+                    configuration.GetConnectionString("DefaultConnection")));
+
+            return services;
+        }
+    }
+}
